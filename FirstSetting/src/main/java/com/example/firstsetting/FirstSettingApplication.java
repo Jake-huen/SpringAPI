fb53fe871395be6183f9cd8a45b32aa9b064bafb
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-04-28 23:15:07 +0900

    Spring First Setting

---
+++ FirstSetting/src/main/java/com/example/firstsetting/FirstSettingApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstsetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSettingApplication.class, args);
+	}
+
+}
---
+++ FirstSetting/src/test/java/com/example/firstsetting/FirstSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstsetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
